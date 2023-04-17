17a40b3b3627351b5b8cc34477cd69ca8715f0aa
Aidan Chien <118775203+chieaid24@users.noreply.github.com>
2023-04-17 14:00:24 -0700

    a

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
+// these create variables that represent the divs / objects in the HTML.
+// These can be changed manipulated using javascript
 const mainMenu = document.querySelector(".main-menu");
 const clickableArea = document.querySelector(".clickable-area");
 const message = document.querySelector(".clickable-area .message");
+const endScreen = document.querySelector(".end-screen");
 
-
+//initize variables
 let timer;
 let greenDisplayed;
 let timeNow;
+let waitingForStart;
+let waitingForGreen;
+let scores;
 
+//the starting screen that is called initially
 const init = () => {
     greenDisplayed = false;
+    waitingForStart = false;
+    waitingForGreen = false;
+    scores = [];
 };
 
 init();
@@ -27,10 +37,11 @@ const startGame = () => {
     message.style.color = "#fff";
 
 
-    let randomNumber = Math.floor(Math.random() * 3500 + 2000);
+    let randomNumber = Math.floor(Math.random() * 2000 + 1500);
     timer = setTimeout(setGreenColor, randomNumber);
-    
-    console.log(randomNumber)
+    waitingForStart = false;
+    waitingForGreen = true;
+    console.log(randomNumber);
 };
 
 mainMenu.addEventListener("click", () => {
@@ -38,11 +49,39 @@ mainMenu.addEventListener("click", () => {
     startGame();
 });
 
+const endGame = () => {
+    endScreen.classList.add("active");
+    clearTimeout(timer);
+
+    let total = 0;
+    scores.forEach((s) => {
+        total += s;
+    })
+
+    let averageScore = Math.round(total / scores.length);
+    console.log("Total" + total);
+    console.log("average score: " + averageScore)
+};
+
 const displayReactionTime = (rt) => {
     clickableArea.style.backgroundColor = "#faf0ca";
-    message.innerHTML = `<div class='reaction-time-text'>${rt} ms</div>Click to continue.
-    `;
+    message.innerHTML = `<div class='reaction-time-text'>${rt} ms</div>Click to continue.`;
     greenDisplayed = false;
+    waitingForStart = true;
+    scores.push(rt);
+    console.log("scores " + scores)
+
+    if (scores.length >= 5) {
+        endGame();
+    }
+}
+
+const displayTooSoon = () => {
+    clickableArea.style.backgroundcolor = "#faf0ca";
+    message.innerHTML = "Too soon. Click to contuneu";
+    message.style.color = "#111";
+    waitingForStart = true;
+    clearTimeout(timer);
 }
 
 clickableArea.addEventListener("click", () => {
@@ -52,5 +91,14 @@ clickableArea.addEventListener("click", () => {
         console.log("reaction time " + reactionTime);
         displayReactionTime(reactionTime);
         return;
+    }
+
+    if (waitingForStart) {
+        startGame();
+        return;
+    }
+
+    if (waitingForGreen) {
+        displayTooSoon();
     }
 });
