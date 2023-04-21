709998439ed02e56584ae70cb310986b6e9e75f2
Aidan Chien <118775203+chieaid24@users.noreply.github.com>
2023-04-21 14:00:44 -0700

    a

--- reaction time/main.js
+++ reaction time/main.js
@@ -4,6 +4,8 @@ const mainMenu = document.querySelector(".main-menu");
 const clickableArea = document.querySelector(".clickable-area");
 const message = document.querySelector(".clickable-area .message");
 const endScreen = document.querySelector(".end-screen");
+const reactionTimeText = document.querySelector(".end-screen .reaction-time-text");
+const playAgainBtn = document.querySelector(".end-screen .play-again-btn")
 
 //initize variables
 let timer;
@@ -35,47 +37,59 @@ const setGreenColor = () => {
     timeNow = Date.now();
 }
 
+//initializes the clickable "wait for green" screen
 const startGame = () => {
     clickableArea.style.backgroundColor = "#c1121f";
     message.innerHTML = "Wait for green color";
     message.style.color = "#fff";
 
-
+    //generate random number
     let randomNumber = Math.floor(Math.random() * 2000 + 1500);
+    //when random number time passes, call setGreenColor method
     timer = setTimeout(setGreenColor, randomNumber);
     waitingForStart = false;
     waitingForGreen = true;
-    console.log(randomNumber);
 };
 
+//when on mainmenu and clicked, removed the active class from mainmenu
 mainMenu.addEventListener("click", () => {
     mainMenu.classList.remove("active");
     startGame();
 });
 
+//method ends the game, shows end screen, and calculates average reaction
+//time
 const endGame = () => {
     endScreen.classList.add("active");
     clearTimeout(timer);
 
+    //add all scores together
     let total = 0;
     scores.forEach((s) => {
         total += s;
     })
 
+    //create average score
     let averageScore = Math.round(total / scores.length);
     console.log("Total" + total);
     console.log("average score: " + averageScore)
+
+    //change innerHTML to the variable with this syntax
+    reactionTimeText.innerHTML = `${averageScore} ms`;
 };
 
+//after every trial, this is shown
 const displayReactionTime = (rt) => {
     clickableArea.style.backgroundColor = "#faf0ca";
+    //create a new div? that shows the reaction time as a variable,
+    //changes "click now" text to "click to continue"
     message.innerHTML = `<div class='reaction-time-text'>${rt} ms</div>Click to continue.`;
     greenDisplayed = false;
     waitingForStart = true;
     scores.push(rt);
     console.log("scores " + scores)
 
-    if (scores.length >= 5) {
+    if (scores.length >= 2) {
         endGame();
     }
 }
@@ -106,3 +120,11 @@ clickableArea.addEventListener("click", () => {
         displayTooSoon();
     }
 });
+
+playAgainBtn.addEventListener("click", () => {
+    endScreen.classList.remove("active");
+    init();
+    waitingForStart = true;
+    mainMenu.classList.add("active");
+    //startGame();
+})
